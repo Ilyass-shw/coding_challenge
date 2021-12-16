d207ddf815235a4294874f4e23a05510f503a3b7
ilyass-shw <souwain.malak@gmail.com>
2021-12-16 14:26:13 +0100

    set up msw and add the getStartups method

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,21 @@
-import { Fragment, ReactElement } from "react";
+import { Fragment, ReactElement, useEffect, useState } from "react";
+import { StartupHttpService } from "../../Http/Startup/Startup.http.service";
+import { Startup } from "../../Types/Startup";
 
 export default function StartupList(): ReactElement {
+  const [startups, setStartups] = useState<Startup[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const list = await StartupHttpService.getStartups();
+        setStartups(list);
+        console.log(list);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
+
   return <Fragment></Fragment>;
 }
